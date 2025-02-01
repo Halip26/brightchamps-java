24079ed58b4fd62cfadb17e386c752ab1b9e4700
Halip26 <77138807+Halip26@users.noreply.github.com>
2025-02-01 21:01:54 +0800

    update the codes with more simple

--- src/BMIcalculator.java
+++ src/BMIcalculator.java
@@ -1,29 +1,37 @@
 import java.util.Scanner; // mengimport scanner yang digunakan untuk meminta input dari user
 
-public class BMIcalculator { // nama class
-    public static void main(String[] args) { // method utama
-        try (Scanner sc = new Scanner(System.in)) {
+public class BMIcalculator {
+    private static Scanner scan;
 
-            System.out.print("Masukkan berat badan (kg): "); // output kalimat untuk meminta input berat badan
-            double weight = sc.nextDouble(); // mengambil input berat badan dari user
-            System.out.print("Masukkan tinggi badan (cm): "); // output kalimat untuk meminta input tinggi badan
-            double height = sc.nextDouble(); // mengambil input tinggi badan dari user
-            double bmi = weight / (height * height); // menghitung BMI
+    public static void main(String[] args) {
+        scan = new Scanner(System.in);
+        System.out.print("\nMasukkan berat badan (kg): ");
+        double weight = scan.nextDouble();
+        System.out.print("Masukkan tinggi badan (cm): ");
+        double height = scan.nextDouble();
 
-            // menampilkan hasil perhitungan BMI dalam If statement
-            if (bmi < 15.0) {
-                System.out.println("BMI Anda sangat kurus");
-            } else if (bmi >= 15.0 && bmi < 16.0) {
-                System.out.println("BMI Anda kurus");
-            } else if (bmi >= 16.0 && bmi < 18.5) {
-                System.out.println("BMI Anda sedang");
-            } else if (bmi >= 18.5 && bmi < 25.0) {
-                System.out.println("BMI Anda normal (ideal)");
-            } else if (bmi >= 25.0 && bmi < 30.0) {
-                System.out.println("BMI Anda berat");
-            } else {
-                System.out.println("BMI Anda sangat berat (obesitas)");
-            }
+        double bmi = weight / ((height * height) / 10000);
+
+        // create lines using looping for
+        for (int i = 0; i <= 35; i++) {
+            System.out.print("-");
         }
+        System.out.println();
+
+        System.out.printf("BMI anda adalah: %.2f - ", bmi);
+
+        // show the results
+        if (bmi < 15.0) {
+            System.out.println("Anda sangat kurus (Very uderweight)");
+        } else if (bmi >= 15.0 && bmi < 16.0) {
+            System.out.println("Anda kurus (Uderweight)");
+        } else if (bmi >= 18.5 && bmi < 25.0) {
+            System.out.println("Anda normal (Ideal)");
+        } else if (bmi >= 25.0 && bmi < 30.0) {
+            System.out.println("Anda berat (Overweight)");
+        } else {
+            System.out.println("Anda sangat berat (Obese)");
+        }
+
     }
 }
