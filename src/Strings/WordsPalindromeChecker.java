bc4f0ad358d0b504cce50ed19badb27e1d7133d1
Halip26 <77138807+Halip26@users.noreply.github.com>
2025-03-09 15:59:20 +0800

    Refactor WordsPalindromeChecker.java to simplify palindrome checking by removing unnecessary string cleaning method

--- src/Strings/WordsPalindromeChecker.java
+++ src/Strings/WordsPalindromeChecker.java
@@ -5,28 +5,15 @@ import java.util.Scanner;
 public class WordsPalindromeChecker {
   public static Scanner scan;
 
-  // Method or function to remove spaces and convert letters to lowercase
-  public static String cleanString(String str) {
-    StringBuilder result = new StringBuilder();
-    for (char ch : str.toCharArray()) {
-      if (Character.isAlphabetic(ch)) {
-        result.append(Character.toLowerCase(ch));
-      }
-    }
-    return result.toString();
-  }
-
   // Method or Function to check if a word is a palindrome
   public static boolean isPalindrome(String word) {
+    String moreWords = word.replaceAll("\\s+", "").toLowerCase();
 
-    String cleanStr = cleanString(word);
-    String reversedStr = cleanStr;
-
-    int length = reversedStr.length();
+    int length = moreWords.length();
 
     // looping to compare characters
     for (int i = 0, j = length - 1; i < j; i++, j--) {
-      if (reversedStr.charAt(i) != reversedStr.charAt(j)) {
+      if (moreWords.charAt(i) != moreWords.charAt(j)) {
         return false; // not a palindrome
       }
     }
